a11db4c4dca82fb5b0ad507bdf87f1a63ac581bf
ajiana01 <ajiananta177013@students.unnes.ac.id>
2023-08-27 15:49:58 +0700

    update baru

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,10 @@ import Alert from 'bootstrap/js/dist/alert';
 
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap';
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
